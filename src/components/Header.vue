7ab6baa1a8681ab141896df1f34e9160c56f0d84
Michael Carter <mc0202480@otc.edu>
2020-11-30 14:58:24 -0600

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: ['title']
+}
+</script>
+
+<style scoped>
+    .header
+        {
+            background: #333;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            margin: 0px;
+        }
+</style>
